ad71b23223e8e8a9fcfd25781459dfb9784af2fd
DearRabbit <DearRabbit@users.noreply.github.com>
2016-06-19 01:09:49 +0800

    alpha: finish api

--- db/clam.sql
+++ db/clam.sql
@@ -39,6 +39,7 @@ CREATE TABLE `friendList` (
 
 LOCK TABLES `friendList` WRITE;
 /*!40000 ALTER TABLE `friendList` DISABLE KEYS */;
+INSERT INTO `friendList` VALUES (4,8,'atfriend'),(8,4,'ä¸­æ'),(4,11,'at4'),(11,4,'中文');
 /*!40000 ALTER TABLE `friendList` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -50,11 +51,13 @@ DROP TABLE IF EXISTS `msgLog`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `msgLog` (
+  `msgid` int(11) NOT NULL AUTO_INCREMENT,
   `senderID` int(11) DEFAULT NULL,
   `recverID` int(11) DEFAULT NULL,
   `msgState` int(11) DEFAULT NULL,
-  `sendTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `sendTime` datetime DEFAULT NULL,
   `msgContent` text,
+  PRIMARY KEY (`msgid`),
   KEY `senderID` (`senderID`),
   KEY `recverID` (`recverID`),
   CONSTRAINT `msgLog_ibfk_1` FOREIGN KEY (`senderID`) REFERENCES `userinfo` (`userID`) ON DELETE CASCADE,
@@ -84,14 +87,14 @@ CREATE TABLE `userinfo` (
   `passWord` char(32) NOT NULL,
   `salt` char(32) NOT NULL,
   `userState` int(11) DEFAULT NULL,
-  `registerDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `registerDate` datetime DEFAULT NULL,
   `realName` varchar(40) DEFAULT NULL,
   `email` varchar(50) DEFAULT NULL,
   `phone` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`userID`),
   UNIQUE KEY `userName` (`userName`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -100,7 +103,7 @@ CREATE TABLE `userinfo` (
 
 LOCK TABLES `userinfo` WRITE;
 /*!40000 ALTER TABLE `userinfo` DISABLE KEYS */;
-INSERT INTO `userinfo` VALUES (1,'username1','pass','salt',1,'2016-06-18 10:55:59','lee','e@zju.com','1230');
+INSERT INTO `userinfo` VALUES (3,'username1','pass','salt',0,'2016-06-18 13:51:37','lee','e@zju.com','1230'),(4,'usr2','pwd','salt',0,'2016-06-18 13:51:37','lee2','4@zju.com','123'),(8,'user1','17cb4497c29517e91558f55e12949a69','fe9d26c3e620eeb69bd166c8be89fb8f',0,'2016-06-18 13:10:06','lee','jj@qq.com','123'),(11,'us1','17cb4497c29517e91558f55e12949a69','fe9d26c3e620eeb69bd166c8be89fb8f',1,'2016-06-18 13:11:02','lee','jj@qqxin.com','123');
 /*!40000 ALTER TABLE `userinfo` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -113,4 +116,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-06-18 18:56:39
+-- Dump completed on 2016-06-18 15:51:51
--- routes/api.js
+++ routes/api.js
@@ -1,14 +1,72 @@
 var express = require('express');
 var conn = require('../db/connect');
+var crypto = require('crypto');
+
 var router = express.Router();
 
 sqlSet = {
   getUserInfo: "select userID, userName, userState, registerDate, realName, email, phone from userinfo where userID = ?",
-
   getFriendInfo: "",
-  register: "insert into userinfo(userName, passWord, salt, userState, realName, email, phone) values (?, ?, ?, ?, ?, ?, ?)",
+  getFriendList: "select * from friendList where mainID = ?",
+  setGroupName: "update friendList set groupName = ? where mainID = ? and friendID = ?",
+
+  validation: "select count(userID) as result from userinfo where userName = ? or email = ?",
+  register: "insert into userinfo(userName, passWord, salt, userState, realName, email, phone) values (?, ?, ?, 0, ?, ?, ?)",
+
+  sendMsg: "insert into msgLog(senderID, recverID, msgState, msgContent) values (?, ?, 0, ?)",
+  recvMsg: "select * from msgLog where msgState = 0 and recverID = ?",
+
+  insertFriend: "insert into friendList values (?, ?, ?)",
+  deleteFriend: "delete from friendList where mainID = ? and friendID = ?",
+
+  setOnline: "update userinfo set userState = 1 where userID = ?",
+  setOfffine: "update userinfo set userState = 0 where userID = ?",
 };
 
+var failRet = {result: -1};
+var succRet = {result: 0 };
+
+var waitingList = {};
+// Note:
+// 1:{2:'name', 3:'gp2'}, 2:{3:'gp1'}}
+// for userID = 1, No.2 wants to add him as group 'name'
+// No.3 wants to add him as group 'gp2'.
+
+function insertFriend(mainID, friendID, groupName) {
+  conn.getConnection(function (err, conn) {
+    if (err) {
+      console.log("POOL ==> " + err);
+    }
+
+    else {
+      conn.query(sqlSet.insertFriend, [mainID, friendID, groupName], function(err, rows){
+        if (err) {
+          console.log(err);
+        }
+        conn.release();
+      });
+    }
+  });
+}
+
+function deleteFriend (mainID, friendID) {
+  conn.getConnection(function (err, conn) {
+    if (err) {
+      console.log("POOL ==> " + err);
+    }
+
+    else {
+      conn.query(sqlSet.deleteFriend, [mainID, friendID], function(err, rows){
+        if (err) {
+          console.log(err);
+        }
+          conn.release();
+      });
+    }
+  });
+}
+
+// user management
 router.post('/getUserInfo', function (req, res, next) {
   conn.getConnection(function (err, conn) {
     if (err) {
@@ -19,39 +77,220 @@ router.post('/getUserInfo', function (req, res, next) {
       conn.query(sqlSet.getUserInfo, [req.query.id], function(err,rows){
         if (err) {
           console.log(err);
-        }
-        conn.release();
-
-        if (rows.length == 1) {
-          console.log(rows[0].registerDate);
-          return res.json(rows[0]);
+          conn.release();
+          return res.json({});
         }
         else {
-          return res.json({});
+          conn.release();
+          if (rows.length == 0) {
+            return res.json({});
+          }
+          return res.json(rows[0]);
         }
       });
     }
   });
 });
 
-router.post('/getFriendInfo', function (req, res, next) {
+router.post('/getFriendList', function (req, res, next) {
   conn.getConnection(function (err, conn) {
     if (err) {
       console.log("POOL ==> " + err);
     }
 
     else {
-      conn.query(sqlSet.getFriendInfo, [req.query.id], function(err,rows){
+      conn.query(sqlSet.getFriendList, [req.query.id], function(err,rows){
+        conn.release();
         if (err) {
           console.log(err);
+          return res.json({});
         }
-        conn.release();
-
         return res.json(rows);
       });
     }
   });
 });
 
+router.post('/register', function (req, res, next) {
+  conn.getConnection(function (err, conn) {
+    if (err) {
+      console.log("POOL ==> " + err);
+    }
+
+    else {
+      var md5_1 = crypto.createHash('md5');
+      md5_1.update(req.query.password);
+
+      var pwdInMD5 = md5_1.digest('hex');
+      var salt = 'fe9d26c3e620eeb69bd166c8be89fb8f';
+      var withSalt = pwdInMD5 + salt;
+
+      var md5_2 = crypto.createHash('md5');
+      md5_2.update(withSalt);
+      var pwdSalt = md5_2.digest('hex');
+
+      var qSet = [req.query.userName, pwdSalt, salt, req.query.realName, req.query.email, req.query.phone];
+      conn.query(sqlSet.register, qSet, function(err,rows){
+        conn.release();
+        if (err) {
+          console.log(err);
+          return res.json(failRet);
+        }
+        return res.json(succRet);
+      });
+    }
+  });
+});
+
+router.post('/validation', function (req, res, next) {
+  conn.getConnection(function (err, conn) {
+    if (err) {
+      console.log("POOL ==> " + err);
+    }
+
+    else {
+      conn.query(sqlSet.validation, [req.query.userName, req.query.email], function(err,rows){
+        conn.release();
+        if (err) {
+          console.log(err);
+          return res.json(failRet);
+        }
+        return res.json(rows[0]);//{result:0}
+      });
+    }
+  });
+});
+
+router.post('/askAddFriend', function (req, res, next) {
+  if (waitingList[req.query.friendID] == undefined) {
+    waitingList[req.query.friendID] = {};
+  }
+  waitingList[req.query.friendID][req.query.mainID] = req.query.groupName;
+
+  return res.json(succRet);
+});
+
+router.post('/getAddFriendList', function (req, res, next) {
+  if (waitingList[req.query.id] == undefined) {
+    return res.json({});
+  }
+  else {
+    return res.json(waitingList[req.query.id]);
+  }
+});
+
+router.post('/confirmAddFriend', function (req, res, next) {
+  var senderID = req.query.senderID;
+  var recverID = req.query.recverID;
+
+  if (waitingList[recverID]) {
+    if (waitingList[recverID][senderID]) {
+      insertFriend(senderID, recverID, waitingList[recverID][senderID]);
+      insertFriend(recverID, senderID, req.query.groupName);
+      return res.json(succRet);
+    }
+    delete waitingList[recverID][senderID];
+  }
+  return res.json(failRet);
+});
+
+router.post('/deleteFriend', function (req, res, next) {
+  deleteFriend(req.query.mainID, req.query.friendID);
+});
+
+router.post('/sendMsg', function (req, res, next) {
+  conn.getConnection(function (err, conn) {
+    if (err) {
+      console.log("POOL ==> " + err);
+    }
+
+    else {
+      conn.query(sqlSet.sendMsg, [req.query.senderID, req.query.recverID, req.query.msgContent], function(err, rows){
+        conn.release();
+        if (err) {
+          console.log(err);
+          return res.json(failRet);
+        }
+        return res.json(succRet);
+      });
+    }
+  });
+});
+
+router.post('/recvMsg', function (req, res, next) {
+  conn.getConnection(function (err, conn) {
+    if (err) {
+      console.log("POOL ==> " + err);
+    }
+
+    else {
+      conn.query(sqlSet.sendMsg, [req.query.id], function(err, rows){
+        conn.release();
+        if (err) {
+          console.log(err);
+          return res.json({});
+        }
+        return res.json(rows);
+      });
+    }
+  });
+});
+
+router.post('/setGroupName', function (req, res, next) {
+  conn.getConnection(function (err, conn) {
+    if (err) {
+      console.log("POOL ==> " + err);
+    }
+
+    else {
+      conn.query(sqlSet.setGroupName, [req.query.groupName, req.query.mainID, req.query.friendID], function(err, rows){
+        conn.release();
+        if (err) {
+          console.log(err);
+          return res.json(failRet);
+        }
+        return res.json(succRet);
+      });
+    }
+  });
+});
+
+router.post('/setOnline', function (req, res, next) {
+  conn.getConnection(function (err, conn) {
+    if (err) {
+      console.log("POOL ==> " + err);
+    }
+
+    else {
+      conn.query(sqlSet.setOnline, [req.query.id], function(err, rows){
+        conn.release();
+        if (err) {
+          console.log(err);
+          return res.json(failRet);
+        }
+        return res.json(succRet);
+      });
+    }
+  });
+});
+
+router.post('/setOffline', function (req, res, next) {
+  conn.getConnection(function (err, conn) {
+    if (err) {
+      console.log("POOL ==> " + err);
+    }
+
+    else {
+      conn.query(sqlSet.setOfffine, [req.query.id], function(err, rows){
+        conn.release();
+        if (err) {
+          console.log(err);
+          return res.json(failRet);
+        }
+        return res.json(succRet);
+      });
+    }
+  });
+});
 
 module.exports = router;
