d8940df29a98d6b340f315605ebee56dc0491676
DearRabbit <DearRabbit@users.noreply.github.com>
2016-06-19 01:18:18 +0800

    delete useless

--- app.js
+++ app.js
@@ -4,10 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var session = require('express-session');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
 var api = require('./routes/api');
 
 var app = express();
@@ -24,16 +20,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(session({
-  secret: 'a459f596cd10615b',
-  cookie: { maxAge: 600000 },
-  resave: true,
-  saveUninitialized: false
-}));
-
-app.use('/', routes);
 app.use('/api', api);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
